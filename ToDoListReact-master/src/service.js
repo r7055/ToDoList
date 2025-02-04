f54bc6695cfdcfd0e95b9393f4cf7631ceea3dab
r7055 <ruchami7055@gamil.com>
2025-02-04 23:27:41 +0200

    log the url

--- ToDoListReact-master/src/service.js
+++ ToDoListReact-master/src/service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = process.env.REACT_APP_API_URL;
-
+console.log(process.env.REACT_APP_API_URL)
 
 axios.interceptors.response.use(
   response => response, 
